bab549d8c99e7630d36a1a6edc2c8e6403a60fd7
latimeks <clatimer@revspringinc.com>
2019-08-12 15:22:44 -0400

    Cleanup

--- wav_splicer/index.js
+++ wav_splicer/index.js
@@ -1,12 +1,3 @@
-/*
-  Make script to do scp easily
-  Improve comments
-  Improve logging
-  Make documentation
-  Add buffer to prevent file concat, it should give an option then give a full overwrite
-  Prevent duplicate scp?? How does scp handle copying  to the same file
-*/
-
 const fs = require('fs');
 const cliArgs = process.argv.slice(2);
 const inputFile = 'input/' + cliArgs[0];
@@ -18,8 +9,10 @@ newWavNameArr[2] += ".wav";
 let outputFile = 'output/' + user + "-" + newWavNameArr[0] + "-wavs.txt";
 let outputData = "";
 let outputCount = 0;
+
 console.log(`Splicing ${newWavName} into ${inputFile} for account #: ${newWavNameArr[0]}`);
 console.log(`Processing...`);
+
 var fileData = fs.readFileSync(inputFile, 'utf-8').split(/\r?\n/).forEach(function(line){
   console.log(`Line from text file: ${line}`);
   if(line!=''){
@@ -31,7 +24,7 @@ var fileData = fs.readFileSync(inputFile, 'utf-8').split(/\r?\n/).forEach(functi
   }
 });
 
-fs.appendFile(outputFile, outputData, (err) => {
+fs.appendFileSync(outputFile, outputData, (err) => {
   if(err)
     throw err;
   console.log(`New file: ${outputFile}, containing ${outputCount} total lines`);
